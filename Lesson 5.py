e30c966c5f2f4075136581b24e1071c345359dcd
KrithiTuggudem <tuggudem.krithi@gmail.com>
2020-11-25 15:21:36 -0500

    Lesson 5 Homework (string)

---
+++ Lesson 5.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("String")
+
+
+# In[2]:
+
+
+print("String means to add quotes to words/letters you want python to say")
+
+
+# In[5]:
+
+
+print ("hello")
+
+
+# In[6]:
+
+
+print ("5*5")
+
+
+# In[7]:
+
+
+print (5)
+
+
+# In[8]:
+
+
+print (5*5)
+
+
+# In[ ]:
+
+
+
+
