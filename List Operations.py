6499a8cffab53bcd07bfd5e5e9707dfdc24edda4
KrithiTuggudem <tuggudem.krithi@gmail.com>
2021-01-09 22:23:02 -0500

    Acess List Items, Change List Items, Add List Items

---
+++ List Operations.py
@@ -0,0 +1,34 @@
+#1. 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+
+#1.1 Print fourth, and fifth item from the list
+print(thislist[4:6])
+#1.3 print items from the beginning to, but NOT including, "melon"
+print(thislist[ :5])
+#1.3 print item from "banana" to the end
+print(thislist[1: ])
+
+
+#2. 
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+
+#2.1 Change/replace  the value of  "banana" to "orange" in the list.
+thislist[1] = "orange"
+print (thislist)
+#2.2 Change the values "cherry" and   "orange" with the values "blackcurrant" and "watermelon"
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[2:4] = ["blackcurrant, watermelon"]
+print(thislist)
+#2.3 Change the third and fourth value by replacing it with "mango"
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[3:5] = ["mango"]
+print(thislist)
+
+
+#3. 
+thislist = ["apple", "banana", "cherry"]
+
+#3.1 use function to insert "watermelon" as the third item
+
+#3.2 use function to insert "watermelon" as the Last item
+#3.3 use function to insert "watermelon" as the first item
